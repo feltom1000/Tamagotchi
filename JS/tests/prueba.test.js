e48acea995e0d2e29e4d86063cff7b5c8d2dc385
feltom1000 <feltom1000@hotmail.com>
2021-05-14 19:59:06 -0300

    Testing: Cambie los beforeAll por BeforeEach

--- JS/tests/prueba.test.js
+++ JS/tests/prueba.test.js
@@ -10,10 +10,11 @@ const Pescado = require('../pescado')
 describe('La mascota existe', () => {
     let mascota // Variable global
     //beforeAll(()=>{
+      //  mascota = new Mascota
        // });
 
         describe('Cuando la mascota esta muy cansada (energia = 0)', () => {
-            beforeAll(()=>{
+            beforeEach(()=>{
                 mascota = new Mascota()
                 mascota.energia = 0
                 });
@@ -38,108 +39,106 @@ describe('La mascota existe', () => {
         });
         
         describe('Mascota intenta tomar la medicina', () => {
-            beforeAll(()=>{
-                mascota0 = new Mascota()
+            beforeEach(()=>{
+                mascota = new Mascota()
                 });
 
-            describe('Y al tener mas de 60 de salud', () => {
-                it('devuelve 100', () => {
-                    mascota0.salud = 65
-                    mascota0.tomarMedicina()
-                    expect(mascota0.salud).toEqual(100)
-                    expect(mascota0.energia).toEqual(90)
-                    expect(mascota0.felicidad).toEqual(85)
+                describe('Y al tener mas de 60 de salud', () => {
+                    it('devuelve 100', () => {
+                        mascota.salud = 65
+                        mascota.tomarMedicina()
+                        expect(mascota.salud).toEqual(100)
+                        expect(mascota.energia).toEqual(90)
+                        expect(mascota.felicidad).toEqual(85)
+                    });
                 });
-            });
             
-            describe('Y al no tener salud', () => {
-                it('suma 40 de salud', () => {
-                    let mascota5 = new Mascota
-                    mascota5.salud = 0
-                    mascota5.tomarMedicina()
-                    expect(mascota5.salud).toEqual(40)
-                    expect(mascota5.energia).toEqual(90)
-                    expect(mascota5.felicidad).toEqual(85)
+                describe('Y al no tener salud', () => {
+                    it('suma 40 de salud', () => {
+                        mascota.salud = 0
+                        mascota.tomarMedicina()
+                        expect(mascota.salud).toEqual(40)
+                        expect(mascota.energia).toEqual(90)
+                        expect(mascota.felicidad).toEqual(85)
+                    });
                 });
-            });
         });
         
         describe('Mascota intenta ir al baño', () => {
-            beforeAll(()=>{
-                mascota1 = new Mascota()
+            beforeEach(()=>{
+                mascota = new Mascota()
                 });
 
-            describe('Y al tener + de 95 de Felicidad', () => {
-                it('devuelve 100', () => {
-                    mascota1.felicidad = 96
-                    mascota1.irAlBanio()
-                    expect(mascota1.felicidad).toEqual(100)
+                describe('Y al tener + de 95 de Felicidad', () => {
+                    it('devuelve 100', () => {
+                        mascota.felicidad = 96
+                        mascota.irAlBanio()
+                        expect(mascota.felicidad).toEqual(100)
+                    });
                 });
-            });
             
-            describe('Y al no tener felicidad', () => {
-                it('suma 5', () => {
-                    mascota1.felicidad = 0
-                    mascota1.irAlBanio()
-                    expect(mascota1.felicidad).toEqual(5)
+                describe('Y al no tener felicidad', () => {
+                    it('suma 5', () => {
+                        mascota.felicidad = 0
+                        mascota.irAlBanio()
+                        expect(mascota.felicidad).toEqual(5)
+                    });
                 });
-            });
 
-            describe('Y al tener menos de 45 de Incontinencia', () => {
-                it('incontinencia devuelve 0', () => {
-                    mascota1.incontinencia = 30
-                    mascota1.irAlBanio()
-                    expect(mascota1.incontinencia).toEqual(0)
+                describe('Y al tener menos de 45 de Incontinencia', () => {
+                    it('incontinencia devuelve 0', () => {
+                        mascota.incontinencia = 30
+                        mascota.irAlBanio()
+                        expect(mascota.incontinencia).toEqual(0)
+                    });
                 });
-            });
 
-            describe('Y al tener mas de 45 de Incontinencia', () => {
-                it('incontinencia resta 45', () => {
-                    mascota1.incontinencia = 50
-                    mascota1.irAlBanio()
-                    expect(mascota1.incontinencia).toEqual(5)
+                describe('Y al tener mas de 45 de Incontinencia', () => {
+                    it('incontinencia resta 45', () => {
+                        mascota.incontinencia = 50
+                        mascota.irAlBanio()
+                        expect(mascota.incontinencia).toEqual(5)
+                    });
                 });
-            });
 
-            it('Responde correctamente', () => {
-                expect(mascota1.irAlBanio()).toEqual("Ufff, ya me siento mejor")
-            });
+                it('Responde correctamente', () => {
+                    expect(mascota.irAlBanio()).toEqual("Ufff, ya me siento mejor")
+                });
         });
 
         describe('Mascota intenta jugar', () => {
-            beforeAll(()=>{
-                mascota2 = new Mascota()
+            beforeEach(()=>{
+                mascota = new Mascota()
                 });
 
-            describe('Y al tener mas de 75 de felicidad', () => {
-                it('devuelve 100', () => {
-                    mascota2.felicidad = 80
-                    mascota2.jugar()
-                    expect(mascota2.felicidad).toEqual(100)
+                describe('Y al tener mas de 75 de felicidad', () => {
+                    it('devuelve 100', () => {
+                        mascota.felicidad = 80
+                        mascota.jugar()
+                        expect(mascota.felicidad).toEqual(100)
+                    });
                 });
-            });
             
-            describe('Y al tener menos de 75 de felicidad', () => {
-                it('suma 25', () => {
-                    mascota2.felicidad = 50
-                    mascota2.jugar()
-                    expect(mascota2.felicidad).toEqual(75)
+                describe('Y al tener menos de 75 de felicidad', () => {
+                    it('suma 25', () => {
+                        mascota.felicidad = 50
+                        mascota.jugar()
+                        expect(mascota.felicidad).toEqual(75)
+                    });
                 });
-            });
 
-            it('la energia, la suciedad, la incontinencia y el hambre se alteran.', () => {
-                let mascota4 = new Mascota()
-                mascota4.jugar()
-                expect(mascota4.suciedad).toEqual(25) // Suma 25 de Suciedad
-                expect(mascota4.incontinencia).toEqual(10) // Suma 10 de incontinencia
-                expect(mascota4.energia).toEqual(75) // Baja 25 de Energia
-                expect(mascota4.hambre).toEqual(85) // Baja 25 de hambre
-            });
+                it('la energia, la suciedad, la incontinencia y el hambre se alteran.', () => {
+                    mascota.jugar()
+                    expect(mascota.suciedad).toEqual(25) // Suma 25 de Suciedad
+                    expect(mascota.incontinencia).toEqual(10) // Suma 10 de incontinencia
+                    expect(mascota.energia).toEqual(75) // Baja 25 de Energia
+                    expect(mascota.hambre).toEqual(85) // Baja 25 de hambre
+                });
 
-            it('Responde correctamente al jugar', () => {
-                mascota.jugar()
-                expect(mascota2.jugar()).toEqual("Wiiiiiiiiiiiiiii")
-            });
+                it('Responde correctamente al jugar', () => {
+                    mascota.jugar()
+                    expect(mascota.jugar()).toEqual("Wiiiiiiiiiiiiiii")
+                });
         });
 
         describe('Mascota come:', () => {
@@ -214,58 +213,60 @@ describe('La mascota existe', () => {
         });
 
         describe('Mascota intenta dormir', () => {
-            beforeAll(()=>{
+            beforeEach(()=>{
                 mascota = new Mascota()
                 });
-            describe('Y al tener 100 de energia', () => {
-                it('No puede dormir', () => {
-                    mascota.energia = 100
-                    expect(mascota.dormir()).toEqual("¡¡¡No tengo Sueño!!!!")
-                });
-            });
-            
-            describe('Y al tener mas de 75 de energia', () => {
-                it('devuelve 100 y responde', () => {
-                    mascota.energia = 80
-                    expect(mascota.dormir()).toEqual("zzzZZZzzz")
-                    expect(mascota.energia).toEqual(100)
-                });
-            });
 
-            describe('Y al tener menos de 75 de energia', () => {
-                it('suma 25 y responde', () => {
-                    mascota.energia = 50
-                    expect(mascota.dormir()).toEqual("zzzzzzzzZZZZZZZZZzzzzzzzz")
-                    expect(mascota.energia).toEqual(75)
+                describe('Y al tener 100 de energia', () => {
+                    it('No puede dormir', () => {
+                        mascota.energia = 100
+                        expect(mascota.dormir()).toEqual("¡¡¡No tengo Sueño!!!!")
+                    });
                 });
-            });
+            
+                describe('Y al tener mas de 75 de energia', () => {
+                    it('devuelve 100 y responde', () => {
+                        mascota.energia = 80
+                        expect(mascota.dormir()).toEqual("zzzZZZzzz")
+                        expect(mascota.energia).toEqual(100)
+                    });
+                });
+
+                describe('Y al tener menos de 75 de energia', () => {
+                    it('suma 25 y responde', () => {
+                        mascota.energia = 50
+                        expect(mascota.dormir()).toEqual("zzzzzzzzZZZZZZZZZzzzzzzzz")
+                        expect(mascota.energia).toEqual(75)
+                    });
+                });
         });
 
         describe('Mascota intenta bañarse', () => {
-            beforeAll(()=>{
+            beforeEach(()=>{
                 mascota = new Mascota()
                 });
-            describe('Y al no tener suciedad', () => {
-                it('no se quiere bañar', () => {
-                    mascota.suciedad = 0
-                    expect(mascota.baniarse()).toEqual("¡¡Pero no estoy sucio!!")
-                });
-            });
 
-            describe('Y al tener menos de 25 de suciedad', () => {
-                it('devuelve 0 y responde', () => {
-                    mascota.suciedad = 20
-                    expect(mascota.baniarse()).toEqual("Fresco como una lechuga")
-                    expect(mascota.suciedad).toEqual(0)
+                describe('Y al no tener suciedad', () => {
+                    it('no se quiere bañar', () => {
+                        mascota.suciedad = 0
+                        expect(mascota.baniarse()).toEqual("¡¡Pero no estoy sucio!!")
+                    });
                 });
-            });
 
-            describe('Al tener mas de 25 de suciedad', () => {
-                it('se resta 25 y responde', () => {
-                    mascota.suciedad = 50
-                    expect(mascota.baniarse()).toEqual("Ahora si que brillo de limpito =)")
-                    expect(mascota.suciedad).toEqual(25)
+                describe('Y al tener menos de 25 de suciedad', () => {
+                    it('devuelve 0 y responde', () => {
+                        mascota.suciedad = 20
+                        expect(mascota.baniarse()).toEqual("Fresco como una lechuga")
+                        expect(mascota.suciedad).toEqual(0)
+                    });
                 });
-            });
+
+                describe('Al tener mas de 25 de suciedad', () => {
+                    it('se resta 25 y responde', () => {
+                        mascota.suciedad = 50
+                        expect(mascota.baniarse()).toEqual("Ahora si que brillo de limpito =)")
+                        expect(mascota.suciedad).toEqual(25)
+                    });
+                });
         });
 });
